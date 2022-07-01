22fa90c399894ab779dfdc939cded0d6969a1fff
henriquemsleal <henriquemsleal@gmail.com>
2022-06-30 21:59:45 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# metodos-array-javascript
+
+Testing Array Methods Javascript
+
+Com base em aula, por Matheus Battisti - Hora de Codar:
+
+10 MÉTODOS DE ARRAY QUE TODO DESENVOLVEDOR PRECISA CONHECER
---
+++ scripts.js
@@ -0,0 +1,8 @@
+// array de dados
+const data = [
+  { name: 'Matheus', age: 31, sallary: 2000, driverLicense: true },
+  { name: 'João', age: 18, sallary: 1500, driverLicense: false },
+  { name: 'Mariana', age: 22, sallary: 4000, driverLicense: true },
+  { name: 'Pedro', age: 50, sallary: 7200, driverLicense: true },
+  { name: 'Erica', age: 16, sallary: 0, driverLicense: false },
+];
